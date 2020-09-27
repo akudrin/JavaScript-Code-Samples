ce3e5080bd68681afeb1645133a60769c418b817
Andrei Kudrin <andreikudrin@gmail.com>
2020-09-27 11:02:28 +0100

    Secrets of the JavaScript Ninja - A simple callback function example

--- Numbers.js
+++ Numbers.js
@@ -1,12 +1,10 @@
-Return the factorial of the provided integer
-
+//Return the factorial of the provided integer
 
 function factorialize(num) {
   if (num === 0) {
     return 1;
   }
-  
+
   // Recursion!!
   return num * factorialize(num - 1);
-  
 }
