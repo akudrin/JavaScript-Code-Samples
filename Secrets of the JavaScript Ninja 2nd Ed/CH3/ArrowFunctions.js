0528e6538b95ae578158d72208c9f1958118eec7
Andrei Kudrin <andreikudrin@gmail.com>
2020-10-10 16:54:13 +0100

    JS NINJA - Comparing an arrow function and a function expression

---
+++ Secrets of the JavaScript Ninja 2nd Ed/CH3/ArrowFunctions.js
@@ -0,0 +1,9 @@
+var greet = (name) => "Greetings " + name;
+assert(greet("Oishi") === "Greetings Oishi", "Oishi is properly greeted");
+var anotherGreet = function (name) {
+  return "Greetings " + name;
+};
+assert(
+  anotherGreet("Oishi") === "Greetings Oishi",
+  "Again, Oishi is properly greeted"
+);
