08a8df518dae7b2cec7729a794d3db92b1ef6652
Andrei Kudrin <andreikudrin@gmail.com>
2020-10-16 09:48:43 +0100

    JS Ninja - Building a forEach function to demonstrate setting a function context

---
+++ Secrets of the JavaScript Ninja 2nd Ed/CH4/forEachFunction.js
@@ -0,0 +1,11 @@
+function forEach(list, callback) {
+    for (var n = 0; n < list.length; n++) {
+    callback.call(list[n], n);
+    }
+    }
+    var weapons = [ { type: 'shuriken' },
+    { type: 'katana' },
+    { type:'nunchucks' }];
+    forEach(weapons, function(index){
+    assert(this === weapons[index],
+    "Got the expected value of " + weapons[index].type);
