d6ddb4463fe8d353849d46e793db0e724d6d46d8
Andrei Kudrin <andreikudrin@gmail.com>
2020-10-18 10:10:48 +0100

    JS Ninja - closure example intro

---
+++ Secrets of the JavaScript Ninja 2nd Ed/CH5/IntroClosures.js
@@ -0,0 +1,12 @@
+var outerValue = "samurai";
+var later;
+function outerFunction() {
+  var innerValue = "ninja";
+  function innerFunction() {
+    assert(outerValue === "samurai", "I can see the samurai.");
+    assert(innerValue === "ninja", "I can see the ninja.");
+  }
+  later = innerFunction;
+}
+outerFunction();
+later();
