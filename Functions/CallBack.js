df8d89c0e5ee2ef25003921d198b59e949757816
Andrei Kudrin <andreikudrin@gmail.com>
2020-10-24 22:10:20 +0100

    Callback returns result and passes it to the next stage

--- Functions/CallBack.js
+++ Functions/CallBack.js
@@ -27,3 +27,19 @@ function printAllCallback() {
 }
 //prints in order using callbacks
 printAllCallback();
+
+function addString(previous, current, callback) {
+  setTimeout(() => {
+    callback(previous + " " + current);
+  }, Math.floor(Math.random() * 100) + 1);
+}
+function addAll() {
+  addString("", "A", (result) => {
+    addString(result, "B", (result) => {
+      addString(result, "C", (result) => {
+        console.log(result); // Prints out " A B C"
+      });
+    });
+  });
+}
+addAll();
