cd866554ae5deda1e2d4f4a85ca7920c9fdec63c
Andrei Kudrin <andreikudrin@gmail.com>
2020-10-25 10:06:18 +0000

    Asyn/await  return result and passes it to the next stage

--- Functions/Await_Async.js
+++ Functions/Await_Async.js
@@ -14,3 +14,18 @@ async function printAll() {
   await printString("C");
 }
 printAll();
+
+function addString(previous, current) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(previous + " " + current);
+    }, Math.floor(Math.random() * 100) + 1);
+  });
+}
+async function addAll() {
+  let toPrint = await addString("", "A");
+  toPrint = await addString(toPrint, "B");
+  toPrint = await addString(toPrint, "C");
+  console.log(toPrint); // Prints out " A B C"
+}
+addAll();
