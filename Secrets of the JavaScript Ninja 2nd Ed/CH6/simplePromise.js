59878a550f37ffbcafc7813c9fec2e2127c68fc5
Andrei Kudrin <andreikudrin@gmail.com>
2020-11-05 10:48:21 +0000

    JS  Ninja - Creating a simple promise

---
+++ Secrets of the JavaScript Ninja 2nd Ed/CH6/simplePromise.js
@@ -0,0 +1,12 @@
+const ninjaPromise = new Promise((resolve, reject) => {
+  resolve("Hattori");
+  //reject("An error resolving a promise!");
+});
+ninjaPromise.then(
+  (ninja) => {
+    assert(ninja === "Hattori", "We were promised Hattori!");
+  },
+  (err) => {
+    fail("There shouldn't be an error");
+  }
+);
