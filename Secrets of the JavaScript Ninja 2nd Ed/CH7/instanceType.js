c6ecfcaf43f91e6296b942dc0ffc276371b51446
Andrei Kudrin <andreikudrin@gmail.com>
2020-11-15 11:09:33 +0000

    JS  Ninja - Examining the type of an instance and its constructor

---
+++ Secrets of the JavaScript Ninja 2nd Ed/CH7/instanceType.js
@@ -0,0 +1,8 @@
+function Ninja() {}
+const ninja = new Ninja();
+assert(typeof ninja === "object", "The type of the instance is object.");
+assert(ninja instanceof Ninja, "instanceof identifies the constructor.");
+assert(
+  ninja.constructor === Ninja,
+  "The ninja object was created by the Ninja function."
+);
