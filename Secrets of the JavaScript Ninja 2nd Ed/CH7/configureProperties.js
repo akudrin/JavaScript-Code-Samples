51010a0ca094e845f766de8dc81466576408243d
Andrei Kudrin <andreikudrin@gmail.com>
2020-11-17 14:03:24 +0000

    JS  Ninja - Configuring properties

---
+++ Secrets of the JavaScript Ninja 2nd Ed/CH7/configureProperties.js
@@ -0,0 +1,13 @@
+var ninja = {};
+ninja.name = "Yoshi";
+ninja.weapon = "kusarigama";
+Object.defineProperty(ninja, "sneaky", {
+  configurable: false,
+  enumerable: false,
+  value: true,
+  writable: true,
+});
+assert("sneaky" in ninja, "We can access the new property");
+for (let prop in ninja) {
+  assert(prop !== undefined, "An enumerated property: " + prop);
+}
