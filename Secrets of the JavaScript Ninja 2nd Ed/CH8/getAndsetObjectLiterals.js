a0e07dd5849242304570f65d1fbae331d40ab2fe
Andrei Kudrin <andreikudrin@gmail.com>
2020-11-24 14:08:31 +0000

    JS Ninja - Defining getters and setters in object literals

---
+++ Secrets of the JavaScript Ninja 2nd Ed/CH8/getAndsetObjectLiterals.js
@@ -0,0 +1,18 @@
+const ninjaCollection = {
+  ninjas: ["Yoshi", "Kuma", "Hattori"],
+  get firstNinja() {
+    report("Getting firstNinja");
+    return this.ninjas[0];
+  },
+  set firstNinja(value) {
+    report("Setting firstNinja");
+    this.ninjas[0] = value;
+  },
+};
+assert(ninjaCollection.firstNinja === "Yoshi", "Yoshi is the first ninja");
+ninjaCollection.firstNinja = "Hachi";
+assert(
+  ninjaCollection.firstNinja === "Hachi" &&
+    ninjaCollection.ninjas[0] === "Hachi",
+  "Now Hachi is the first ninja"
+);
