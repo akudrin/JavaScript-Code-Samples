417f4c17942798cfc0b6377cb0fe8d2aab7341d2
Andrei Kudrin <andreikudrin@gmail.com>
2020-11-25 22:25:44 +0000

    JS Ninja - Defining getters and setters with Object.defineProperty

---
+++ Secrets of the JavaScript Ninja 2nd Ed/CH8/ObjectdefineProperty.js
@@ -0,0 +1,21 @@
+function Ninja() {
+  let _skillLevel = 0;
+  Object.defineProperty(this, "skillLevel", {
+    get: () => {
+      report("The get method is called");
+      return _skillLevel;
+    },
+    set: (value) => {
+      report("The set method is called");
+      _skillLevel = value;
+    },
+  });
+}
+const ninja = new Ninja();
+assert(
+  typeof ninja._skillLevel === "undefined",
+  "We cannot access a 'private' property"
+);
+assert(ninja.skillLevel === 0, "The getter works fine!");
+ninja.skillLevel = 10;
+assert(ninja.skillLevel === 10, "The value was updated");
