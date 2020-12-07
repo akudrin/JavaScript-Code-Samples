52c882600989fd02c1d23066d2e9ce510ddc2a7a
Andrei Kudrin <andreikudrin@gmail.com>
2020-12-07 13:34:07 +0000

    JS Ninja - Naïve extraction of a weapons array

---
+++ Secrets of the JavaScript Ninja 2nd Ed/CH9/extractionArray.js
@@ -0,0 +1,16 @@
+const ninjas = [
+  { name: "Yagyu", weapon: "shuriken" },
+  { name: "Yoshi", weapon: "katana" },
+  { name: "Kuma", weapon: "wakizashi" },
+];
+const weapons = [];
+ninjas.forEach((ninja) => {
+  weapons.push(ninja.weapon);
+});
+assert(
+  weapons[0] === "shuriken" &&
+    weapons[1] === "katana" &&
+    weapons[2] === "wakizashi" &&
+    weapons.length === 3,
+  "The new array contains all weapons"
+);
