b09a8bcc577fcc6170a112d9ed1445eed77ad8d0
Andrei Kudrin <andreikudrin@gmail.com>
2020-12-09 09:55:52 +0000

    JS Ninja - Finding array items

---
+++ Secrets of the JavaScript Ninja 2nd Ed/CH9/findArray.js
@@ -0,0 +1,26 @@
+const ninjas = [
+  { name: "Yagyu", weapon: "shuriken" },
+  { name: "Yoshi" },
+  { name: "Kuma", weapon: "wakizashi" },
+];
+const ninjaWithWakizashi = ninjas.find((ninja) => {
+  return ninja.weapon === "wakizashi";
+});
+assert(
+  ninjaWithWakizashi.name === "Kuma" &&
+    ninjaWithWakizashi.weapon === "wakizashi",
+  "Kuma is wielding a wakizashi"
+);
+const ninjaWithKatana = ninjas.find((nina) => {
+  return ninja.weapon === "katana";
+});
+assert(
+  ninjaWithKatana === undefined,
+  "We couldn't find a ninja that wields a katana"
+);
+const armedNinjas = ninjas.filter((ninja) => "weapon" in ninja);
+assert(armedNinjas.length === 2, "There are two armed ninjas:");
+assert(
+  armedNinjas[0].name === "Yagyu" && armedNinjas[1].name === "Kuma",
+  "Yagyu and Kuma"
+);
