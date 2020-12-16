dc2896d6e211c020ab230863fcae92bc168d0a57
Andrei Kudrin <andreikudrin@gmail.com>
2020-12-16 10:19:43 +0000

    JS Ninja - Intersection of sets

---
+++ Secrets of the JavaScript Ninja 2nd Ed/CH9/setIntersection.js
@@ -0,0 +1,7 @@
+const ninjas = new Set(["Kuma", "Hattori", "Yagyu"]);
+const samurai = new Set(["Hattori", "Oda", "Tomoe"]);
+const ninjaSamurais = new Set(
+  [...ninjas].filter((ninja) => samurai.has(ninja))
+);
+assert(ninjaSamurais.size === 1, "There's only one ninja samurai");
+assert(ninjaSamurais.has("Hattori"), "Hattori is his name");
