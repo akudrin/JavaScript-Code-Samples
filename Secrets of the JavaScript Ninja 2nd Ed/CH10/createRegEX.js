538af4565ec01a2733d14ab46f83b842ea4f7161
Andrei Kudrin <andreikudrin@gmail.com>
2020-12-18 20:41:46 +0000

    JS Ninja - Two ways to create a compiled regular expression

---
+++ Secrets of the JavaScript Ninja 2nd Ed/CH10/createRegEX.js
@@ -0,0 +1,7 @@
+const re1 = /test/i;
+const re2 = new RegExp("test", "i");
+assert(re1.toString() === "/test/i", "Verify the contents of the expression.");
+assert(re1.test("TesT"), "Yes, it's case-insensitive.");
+assert(re2.test("TesT"), "This one is too.");
+assert(re1.toString() === re2.toString(), "The regular expressions are equal.");
+assert(re1 !== re2, "But they are different objects.");
