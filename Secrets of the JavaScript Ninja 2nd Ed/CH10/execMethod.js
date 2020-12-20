1457ddf3c2053f641e36e913bcda7de14e859893
Andrei Kudrin <andreikudrin@gmail.com>
2020-12-20 15:48:12 +0000

    JS Ninja - Using the exec method to do both capturing and a global search

---
+++ Secrets of the JavaScript Ninja 2nd Ed/CH10/execMethod.js
@@ -0,0 +1,9 @@
+const html = "<div class='test'><b>Hello</b> <i>world!</i></div>";
+const tag = /<(\/?)(\w+)([^>]*?)>/g;
+let match,
+  num = 0;
+while ((match = tag.exec(html)) !== null) {
+  assert(match.length === 4, "Every match finds each tag and 3 captures.");
+  num++;
+}
+assert(num === 6, "3 opening and 3 closing tags found.");
