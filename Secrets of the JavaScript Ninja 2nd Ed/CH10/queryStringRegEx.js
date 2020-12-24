490ef5e194d4d9ccde11c8c1113a5f7dbd13523e
Andrei Kudrin <andreikudrin@gmail.com>
2020-12-24 07:57:02 +0000

    JS Ninja - A technique for compressing a query string

---
+++ Secrets of the JavaScript Ninja 2nd Ed/CH10/queryStringRegEx.js
@@ -0,0 +1,16 @@
+function compress(source) {
+  const keys = {};
+  source.replace(/([^=&]+)=([^&]*)/g, function (full, key, value) {
+    keys[key] = (keys[key] ? keys[key] + "," : "") + value;
+    return "";
+  });
+  const result = [];
+  for (let key in keys) {
+    result.push(key + "=" + keys[key]);
+  }
+  return result.join("&");
+}
+assert(
+  compress("foo=1&foo=2&blah=a&blah=b&foo=3") === "foo=1,2,3&blah=a,b",
+  "Compression is OK!"
+);
