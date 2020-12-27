7e85a294eb4ad66d7cd964bcd10e11a03dd2b97f
Andrei Kudrin <andreikudrin@gmail.com>
2020-12-27 11:11:08 +0000

    JS Ninja - Using AMD to specify a module dependent on jQuery

---
+++ Secrets of the JavaScript Ninja 2nd Ed/CH11/AMDJQuery.js
@@ -0,0 +1,11 @@
+define("MouseCounterModule", ["jQuery"], ($) => {
+  let numClicks = 0;
+  const handleClick = () => {
+    alert(++numClicks);
+  };
+  return {
+    countClicks: () => {
+      $(document).on("click", handleClick);
+    },
+  };
+});
