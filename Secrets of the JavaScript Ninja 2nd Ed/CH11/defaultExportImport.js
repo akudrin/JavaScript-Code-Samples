27e8e4f47ef90a500373efcc0c59b175f6812b53
Andrei Kudrin <andreikudrin@gmail.com>
2020-12-29 14:40:05 +0000

    JS Ninja - Importing a default export ES6

---
+++ Secrets of the JavaScript Ninja 2nd Ed/CH11/defaultExportImport.js
@@ -0,0 +1,9 @@
+import ImportedNinja from "defaultExport.js";
+import { compareNinjas } from "defaultExport.js";
+const ninja1 = new ImportedNinja("Yoshi");
+const ninja2 = new ImportedNinja("Hattori");
+assert(
+  ninja1 !== undefined && ninja2 !== undefined,
+  "We can create a couple of Ninjas"
+);
+assert(!compareNinjas(ninja1, ninja2), "We can compare ninjas");
