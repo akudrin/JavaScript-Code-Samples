5384d8afbbec12945fa0b038cdf5d8c85320e3ac
Andrei Kudrin <andreikudrin@gmail.com>
2021-01-22 14:41:23 +0000

    NextJS is workinf fixed the issue

--- NextJS/projectMongoDB/nextbnb/tailwind.config.js
+++ NextJS/projectMongoDB/nextbnb/tailwind.config.js
@@ -1,11 +1,12 @@
+const colors = require('tailwindcss/colors');
 module.exports = {
-  purge: [],
+  purge: ['./pages/**/*.js', './components/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: { colors: { 'light-blue': colors.lightBlue, cyan: colors.cyan } },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
