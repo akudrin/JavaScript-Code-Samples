4c3789b357874d1efd5a1bea0806742925256193
Andrei Kudrin <andreikudrin@gmail.com>
2021-02-13 10:34:14 +0000

    JS CookBook - Extracting a List from a String Advanced

--- Javascript Cookbook 2nd/1-2.js
+++ Javascript Cookbook 2nd/1-2.js
@@ -4,3 +4,12 @@ var sentence =
 var start = sentence.indexOf(':');
 var end = sentence.indexOf('.', start + 1);
 var listStr = sentence.substring(start + 1, end);
+fruits = listStr.split(',');
+console.log(fruits); // [' cherries', ' oranges', ' apples', ' bananas']
+fruits.forEach(function (elmnt, indx, arry) {
+  arry[indx] = elmnt.trim();
+});
+console.log(fruits);
+fruits = listStr.split(/\s*,\s*/);
+console.log(fruits);
+fruits = sentence.substring(start + 1, end).split(',');
