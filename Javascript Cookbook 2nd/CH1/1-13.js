0be06d130447d894902495356a2a76169e1686ac
Andrei Kudrin <andreikudrin@gmail.com>
2021-02-23 22:39:17 +0000

    JS CookBook - Summing All Numbers in a Table Column

---
+++ Javascript Cookbook 2nd/CH1/1-13.js
@@ -0,0 +1,6 @@
+var sum = 0;
+// use querySelector to find all second table cells
+var cells = document.querySelectorAll('td:nth-of-type(2)');
+for (var i = 0; i < cells.length; i++) {
+  sum += parseFloat(cells[i].firstChild.data);
+}
