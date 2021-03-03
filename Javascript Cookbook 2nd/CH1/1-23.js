b49a7e3ca8bd55927a01e6eab183faf642bc0c2b
Andrei Kudrin <andreikudrin@gmail.com>
2021-03-03 21:55:46 +0000

    JS CookBook - Extracting a Portion of an Array

---
+++ Javascript Cookbook 2nd/CH1/1-23.js
@@ -0,0 +1,23 @@
+var animals = [
+  'elephant',
+  'tiger',
+  'lion',
+  'zebra',
+  'cat',
+  'dog',
+  'rabbit',
+  'goose',
+];
+var domestic = animals.slice(4, 7);
+console.log(domestic); // ['cat','dog','rabbit'];
+
+var mArray = [];
+mArray[0] = ['apple', 'pear'];
+mArray[1] = ['strawberry', 'lemon'];
+mArray[2] = ['lime', 'peach', 'berry'];
+//shallow copy
+var nArray = mArray.slice(1, 2);
+console.log(mArray[1]); // ['strawberry','lemon']
+nArray[0][0] = 'raspberry';
+console.log(nArray[0]); // ['raspberry','lemon']
+console.log(mArray[1]); // ['raspberry','lemon']
