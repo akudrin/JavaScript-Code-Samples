278ed4b4f795357867e789d9cb04bbf09c8ec222
Andrei Kudrin <andreikudrin@gmail.com>
2021-03-23 16:05:40 +0000

    JS CookBook - Inheriting an Object’s Functionality

--- Javascript Cookbook 2nd/CH4/4-2.js
+++ Javascript Cookbook 2nd/CH4/4-2.js
@@ -15,3 +15,24 @@ String.prototype.trim = function () {
 var str2 = 'two';
 console.log(str.exclaim()); // one!
 console.log(str2.exclaim()); // two!
+
+function Tune(title, artist) {
+  this.concatTitleArtist = function () {
+    return title + ' ' + artist;
+  };
+}
+// create instance, print out values
+var happySong = new Tune('Putting on the Ritz', 'Ella Fitzgerald');
+// extend the object
+Tune.prototype.addCategory = function (categoryName) {
+  this.category = categoryName;
+};
+// add category
+happySong.addCategory('Swing');
+// print song out to new paragraph
+var song =
+  'Title and artist: ' +
+  happySong.concatTitleArtist() +
+  ' Category: ' +
+  happySong.category;
+console.log(song);
