10ab5d9594e56f097736cb9a5c863586a1acc392
Andrei Kudrin <andreikudrin@gmail.com>
2021-04-06 21:41:01 +0100

    JS CookBook - Discovering All Images in Articles Using the Selectors API

---
+++ Javascript Cookbook 2nd/CH5/5-4.js
@@ -0,0 +1,6 @@
+var imgs = document.querySelectorAll('img');
+var imgs = document.querySelectorAll('img + p');
+var imgs = document.querySelectorAll('img[alt=""]');
+var imgs = document.querySelectorAll('article img');
+var imgs = document.querySelectorAll('img:not([alt=""])');
+var imgs = document.querySelectorAll('article> img');
