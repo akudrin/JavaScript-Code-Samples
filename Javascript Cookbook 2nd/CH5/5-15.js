00fc986e2636b547da2c017af8b71a13073de9ab
Andrei Kudrin <andreikudrin@gmail.com>
2021-04-15 20:37:16 +0100

    JS CookBook 2nd - Hiding Page Sections

---
+++ Javascript Cookbook 2nd/CH5/5-15.js
@@ -0,0 +1,6 @@
+//You can set the CSS visibility property to hide and show the message:
+msg.style.hidden = 'visible'; // to display
+msg.style.hidden = 'hidden'; // to hide
+//or you can use the CSS display property:
+msg.style.display = 'block'; // to display
+msg.style.display = 'none'; // to remove from display
