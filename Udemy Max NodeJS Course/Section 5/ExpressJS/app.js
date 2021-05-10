91f021011d4c848aaab93866e8ddf0168bbc8f80
Andrei Kudrin <andreikudrin@gmail.com>
2021-05-10 14:11:47 +0100

    Max NodeJS - ExpressJS Middleware

--- Udemy Max NodeJS Course/Section 5/ExpressJS/app.js
+++ Udemy Max NodeJS Course/Section 5/ExpressJS/app.js
@@ -1,18 +1,20 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('in the middleware');
-  next(); //alows the request to continue to the next middleware
+app.use('/', (req, res, next) => {
+  console.log('This code always runs');
+  next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+  console.log('in another the middleware');
+  res.send('<h1>Add product page</h1>');
+});
+
+app.use('/', (req, res, next) => {
   console.log('in another the middleware');
   res.send('<h1>Hello from Express</h1>');
 });
-const server = http.createServer();
 
-server.listen(3000);
+app.listen(3000);
