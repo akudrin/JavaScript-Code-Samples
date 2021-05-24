a8ffa0e5b61464bfb56b19e8437a708abf08031d
Andrei Kudrin <andreikudrin@gmail.com>
2021-05-24 12:57:21 +0100

    Max UdemyNodeJS - ExpressJS Using Helper Function for Navigation

--- Udemy Max NodeJS Course/Section 5/ExpressJS/app.js
+++ Udemy Max NodeJS Course/Section 5/ExpressJS/app.js
@@ -3,6 +3,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const bodyParser = require('body-parser');
 const path = require('path');
+const rootDir = require('../util/path');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,7 +12,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, 'views', '404.html'));
+  res.sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(3000);
