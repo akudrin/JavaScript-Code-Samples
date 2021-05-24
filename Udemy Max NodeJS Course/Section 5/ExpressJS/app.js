9e71bf07e0bff4d557ea881a7189dedf636c57fd
Andrei Kudrin <andreikudrin@gmail.com>
2021-05-24 12:09:54 +0100

    Max UdemyNodeJS - ExpressJS Routers

--- Udemy Max NodeJS Course/Section 5/ExpressJS/app.js
+++ Udemy Max NodeJS Course/Section 5/ExpressJS/app.js
@@ -1,23 +1,13 @@
 const express = require('express');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded);
 
-app.use('/add-product', (req, res, next) => {
-  console.log('in 1st middleware');
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
-});
+app.use(adminRoutes);
 
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Dummuy Response from /</h1>');
-});
+app.use(shopRoutes);
 
 app.listen(3000);
