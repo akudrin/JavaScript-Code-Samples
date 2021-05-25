7aa573f8d92620a409c9b99a3a543b8451d79db8
Andrei Kudrin <andreikudrin@gmail.com>
2021-05-25 13:36:11 +0100

    Jonas UdemyNodeJS - Promises chaining

--- Jonas Udemy NodeJS/Section 5/index.js
+++ Jonas Udemy NodeJS/Section 5/index.js
@@ -1,19 +1,39 @@
 const fs = require('fs');
 const superagent = require('superagent');
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed ${data}`);
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
+
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
       if (err) {
-        console.log(err.message);
+        reject('I could not find that file!');
       }
-      console.log(res.body.message);
-      fs.writeFile('dog-img.txt', res.body.message, (err) => {
-        if (err) {
-          console.log(err.message);
-        }
-        console.log('Random dog image saved');
-      });
+
+      resolve(data);
     });
-});
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) {
+        reject('could not write to file');
+      }
+      resolve('success');
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+    return writeFilePro('dog-img.txt', res.body.message);
+  })
+  .then(() => console.log('Random dog image save to file!'))
+  .catch((err) => {
+    console.log(err);
+  });
