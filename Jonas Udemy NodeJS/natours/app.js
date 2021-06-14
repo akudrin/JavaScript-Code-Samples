fd0e465b06d43eab9d86b06ed5286673c61c248d
Andrei Kudrin <andreikudrin@gmail.com>
2021-06-14 09:26:08 +0100

    Jonas NodeJS - Delete Requests

--- Jonas Udemy NodeJS/natours/app.js
+++ Jonas Udemy NodeJS/natours/app.js
@@ -71,7 +71,7 @@ app.post('/api/v1/tours', (req, res) => {
 app.patch('/api/v1/tours/:id', (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
-      status: 'faile',
+      status: 'fail',
       message: 'Invalid ID',
     });
   }
@@ -81,6 +81,20 @@ app.patch('/api/v1/tours/:id', (req, res) => {
     data: {
       tour: '<Updated tour>',
     },
+  });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
   });
 });
 
