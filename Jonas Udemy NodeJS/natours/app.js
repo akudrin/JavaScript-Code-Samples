99a3d3c3129e9f0377a37f30ad4adf76333d8bd4
Andrei Kudrin <andreikudrin@gmail.com>
2021-06-15 11:16:07 +0100

    Jonas NodeJS - Natours Serving Static Files

--- Jonas Udemy NodeJS/natours/app.js
+++ Jonas Udemy NodeJS/natours/app.js
@@ -8,8 +8,9 @@ const userRouter = require('./routes/userRoutes');
 //MIDDLEWARE
 
 app.use(morgan('dev'));
-
 app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from middleware');
