d371d12dae941074a17caa84d9a96abec0975f7c
Andrei Kudrin <andreikudrin@gmail.com>
2021-06-15 09:32:25 +0100

    Jonas NodeJS - Natours Basic Middleware in Express

--- Jonas Udemy NodeJS/natours/app.js
+++ Jonas Udemy NodeJS/natours/app.js
@@ -4,6 +4,17 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 const port = 3000;
 
 const tours = JSON.parse(
@@ -11,8 +22,10 @@ const tours = JSON.parse(
 );
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours,
