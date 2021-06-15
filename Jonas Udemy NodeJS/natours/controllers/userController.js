e130ca8b8c4f4187723061ebbd1df632f831cdbf
Andrei Kudrin <andreikudrin@gmail.com>
2021-06-15 10:42:43 +0100

    Jonas NodeJS - Natours Better File Structure

---
+++ Jonas Udemy NodeJS/natours/controllers/userController.js
@@ -0,0 +1,29 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'not ready',
+  });
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'not ready',
+  });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'not ready',
+  });
+};
+
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'not ready',
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'not ready',
+  });
+};
