eb3c74e8f45701a4f55aa02d21007c97267002e7
Andrei Kudrin <andreikudrin@gmail.com>
2021-07-06 11:55:17 +0100

    Jonas NodeJS - mongoose create Model object

--- Jonas Udemy NodeJS/natours/server.js
+++ Jonas Udemy NodeJS/natours/server.js
@@ -14,9 +14,43 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log('DB connection successful!');
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tours', tourSchema);
+
+const testTour = new Tour({
+  name: 'Ocean Breeze',
+  rating: 5,
+  price: 150,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR: ', err);
   });
 
 //console.log(app.get('env'));
