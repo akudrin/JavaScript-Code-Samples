c2721992586508785dd7d1fd71afe3e8c85acb89
Andrei Kudrin <andreikudrin@gmail.com>
2021-07-12 13:44:21 +0100

    Max Udemy NodeJS - EJS Intro

--- Udemy Max NodeJS Course/Section 6/app.js
+++ Udemy Max NodeJS Course/Section 6/app.js
@@ -2,13 +2,14 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars')
+//const expressHbs = require('express-handlebars')
 
 const app = express();
 
-app.engine('handlebars',expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout'}))
-app.set('view engine','handlebars');
+//app.engine('handlebars',expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout'}))
+//app.set('view engine','handlebars');
 //app.set('view engine','pug');
+app.set('view engine','ejs');
 app.set('views','views');
 
 
@@ -23,7 +24,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
  // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
- res.status(404).render('404', {pageTitle: 'Page Not Found!'});
+ res.status(404).render('404', {pageTitle: 'Page Not Found!', path: 'Error'});
 });
 
 app.listen(3000);
