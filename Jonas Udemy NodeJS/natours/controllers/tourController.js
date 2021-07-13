19fbb64df807ffb7cb75b5d4308650dce8cfa985
Andrei Kudrin <andreikudrin@gmail.com>
2021-07-13 14:12:41 +0100

    Jonas NodeJS - Natours Reading Documents

--- Jonas Udemy NodeJS/natours/controllers/tourController.js
+++ Jonas Udemy NodeJS/natours/controllers/tourController.js
@@ -1,18 +1,41 @@
 const Tour = require('../models/tourModel');
 
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-  });
+exports.getAllTours = async (req, res) => {
+  //console.log(req.requestTime);
+
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'failed',
+      message: err,
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {},
-  });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    //Tour.findOne({_id: req.paeams.id})
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'failed',
+      message: err,
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
