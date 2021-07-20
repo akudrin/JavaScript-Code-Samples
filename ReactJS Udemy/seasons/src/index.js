e926593437f7586faa2b0f8f48f4ebdabd23bcd7
Andrei Kudrin <andreikudrin@gmail.com>
2021-07-20 11:01:00 +0100

    ReactJS - State Handling Errors

--- ReactJS Udemy/seasons/src/index.js
+++ ReactJS Udemy/seasons/src/index.js
@@ -6,12 +6,29 @@ if (module.hot) {
 }
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    //this is the only time we do direct assignment to this.state
+    this.state = { lat: null, errorMessage: '' };
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
     );
-    return <div>Latitude: </div>;
+  }
+
+  //React says we have to define render!!!
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading!</div>;
   }
 }
 
