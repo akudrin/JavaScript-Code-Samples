f15e23bb705196803dd151aa39056399e4cfb983
akudrin <41680455+akudrin@users.noreply.github.com>
2021-08-01 09:39:38 +0100

    Loading Scripts Asynchronously the HTML5 Way

---
+++ Javascript Cookbook 2nd/CH12/12-2.js
@@ -0,0 +1,2 @@
+<script src="//cdnjs.cloudflare.com/ajax/libs/mathjs/0.26.0/math.min.js" async> </script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min.js" async> </script>
