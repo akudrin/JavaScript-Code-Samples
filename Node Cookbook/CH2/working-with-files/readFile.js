1ab937607b3aac116ffdab07c516fe934ebb8566
Andrei Kudrin <andreikudrin@gmail.com>
2021-10-13 09:04:33 +0100

    Node Cookbook - Managing files with fs module using callbacks

---
+++ Node Cookbook/CH2/working-with-files/readFile.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+const filepath = path.join(process.cwd(), 'hello.txt');
+fs.readFile(filepath, 'utf8', (err, contents) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('File Contents:', contents);
+  const upperContents = contents.toUpperCase();
+  fs.writeFile(filepath, upperContents, function (err) {
+    if (err) throw err;
+    console.log('File updated.');
+  });
+});
