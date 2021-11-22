3581c7be472b8836911d07e965e9933d973d50e6
Andrei Kudrin <andreikudrin@gmail.com>
2021-11-22 20:39:08 +0000

    NodeCookbook - Koa.js views

---
+++ Node Cookbook/CH6/koa-views-app/routes/index.js
@@ -0,0 +1,8 @@
+const router = require('koa-router')();
+router.get('/', async function (ctx, next) {
+  ctx.state = {
+    title: 'Koa.js',
+  };
+  await ctx.render('index');
+});
+module.exports = router;
