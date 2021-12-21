5478d0d2bc3302d45ab9ee4bb0512de4f5f1b47e
ak <41680455+akudrin@users.noreply.github.com>
2021-12-21 18:25:10 +0000

    Nodebook - Jest Setup steps functions

--- Node Cookbook/CH8/testing-with-jest/test/test.js
+++ Node Cookbook/CH8/testing-with-jest/test/test.js
@@ -1,5 +1,22 @@
 const uppercase = require("./../uppercase");
 describe("uppercase", () => {
+  beforeAll(() => {
+    // Runs once before all tests
+    console.log("before all tests");
+  });
+  beforeEach(() => {
+    // Runs before each test
+    console.log("before each test");
+  });
+  afterEach(() => {
+    // Runs after each test
+    console.log("after each test");
+  });
+  afterAll(() => {
+    // Runs after all tests
+    console.log("after all test");
+  });
+
   test("uppercase hello returns HELLO", () => {
     expect(uppercase("hello")).toBe("HELLO");
   });
