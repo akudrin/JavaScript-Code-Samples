421082815670d45092498464eb13c4da7f83f8f5
ak <41680455+akudrin@users.noreply.github.com>
2021-12-24 15:51:00 +0000

    Nodecook - HTTP stub SinonJS

--- Node Cookbook/CH8/stubbing-http-requests/test/tests.js
+++ Node Cookbook/CH8/stubbing-http-requests/test/tests.js
@@ -1,5 +1,17 @@
 const test = require("tape");
+const sinon = require("sinon");
 const github = require("../github");
+const octokitUserData = require("./octokitUserData.js");
+test("Get GitHub user by username", async (t) => {
+  t.plan(3);
+  sinon.stub(github, "getGitHubUser").returns(octokitUserData);
+  const githubUser = await github.getGitHubUser("octokit");
+  t.equal(githubUser.id, 3430433);
+  t.equal(githubUser.login, "octokit");
+  t.equal(githubUser.name, "Octokit");
+});
+
+/*const github = require("../github");
 test("Get GitHub user by username", async (t) => {
   t.plan(3);
   const githubUser = await github.getGitHubUser("octokit");
@@ -7,3 +19,4 @@ test("Get GitHub user by username", async (t) => {
   t.equal(githubUser.login, "octokit");
   t.equal(githubUser.name, "Octokit");
 });
+*/
