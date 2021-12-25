d247eadd639e978df89d1319d85a797876638b91
ak <41680455+akudrin@users.noreply.github.com>
2021-12-25 17:38:36 +0000

    Nodecook - Puppeteer headless

--- Node Cookbook/CH8/using-puppeteer/test/test.js
+++ Node Cookbook/CH8/using-puppeteer/test/test.js
@@ -2,7 +2,7 @@ const assert = require("assert");
 const puppeteer = require("puppeteer");
 
 async function runTest() {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto("https://example.com");
   const title = await page.$eval("h1", (el) => el.innerText);
