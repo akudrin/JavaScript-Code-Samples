1da6aa9353b7407ea768ddcec58d2ad2f1423b10
ak <41680455+akudrin@users.noreply.github.com>
2021-12-31 16:13:46 +0000

    Nodecook - installed bcryptjs instead of bcrypt

--- Node Cookbook/CH9/hashing-with-bcrypt/hash.js
+++ Node Cookbook/CH9/hashing-with-bcrypt/hash.js
@@ -1,5 +1,5 @@
 const password = process.argv[2];
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const saltRounds = 10;
 
 bcrypt.hash(password, saltRounds, (err, hash) => {
