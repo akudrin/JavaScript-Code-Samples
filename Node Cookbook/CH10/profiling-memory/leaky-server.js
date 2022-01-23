9185b90d5566fcda02516b1b58a0f57d5d5cad93
ak <41680455+akudrin@users.noreply.github.com>
2022-01-23 18:06:30 +0000

    Nodecook - Memory leak 3

--- Node Cookbook/CH10/profiling-memory/leaky-server.js
+++ Node Cookbook/CH10/profiling-memory/leaky-server.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
+  server.setMaxListeners(1);
   server.on("connection", () => {
     console.log("connected");
   });
