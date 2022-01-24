a6786e69b73eb637919d62aba407dfe6728e2ea2
ak <41680455+akudrin@users.noreply.github.com>
2022-01-24 18:19:16 +0000

    Nodecook - Optimizing synchronous functions

---
+++ Node Cookbook/CH10/optimize-sync/slow.js
@@ -0,0 +1,12 @@
+function sumOfSquares(maxNumber) {
+  const array = Array.from(Array(maxNumber + 1).keys());
+  console.log(array);
+  return array
+    .map((number) => {
+      return number ** 2;
+    })
+    .reduce((accumulator, item) => {
+      return accumulator + item;
+    });
+}
+module.exports = sumOfSquares;
