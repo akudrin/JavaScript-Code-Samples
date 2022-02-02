d4ac67c9061b3a3c3a3b65870b5f4e63bbc6b10e
ak <41680455+akudrin@users.noreply.github.com>
2022-02-02 19:13:40 +0000

    React16 - Changing State Data in the App.js

--- React16/todo/src/App.js
+++ React16/todo/src/App.js
@@ -7,11 +7,20 @@ export default class App extends Component {
       userName: "Adam",
     };
   }
+
+  changeStateData = () => {
+    this.setState({
+      userName: this.state.userName === "Adam" ? "Bob" : "Adam",
+    });
+  };
   render() {
     return (
       <div>
         <h4 className="bg-primary text-white text-center p-2">To Do List</h4>
         {this.state.userName}'s To Do List
+        <button className="btn btn-primary m-2" onClick={this.changeStateData}>
+          Change
+        </button>
       </div>
     );
   }
