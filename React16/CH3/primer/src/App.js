56c93f34378bd419600735206433d14fbb68d13b
ak <41680455+akudrin@users.noreply.github.com>
2022-02-10 14:38:34 +0000

    React16 - Using the this Keyword in an Expression in the App.js

--- React16/CH3/primer/src/App.js
+++ React16/CH3/primer/src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 4,
+    };
+  }
+  isEven() {
+    return this.state.count % 2 === 0 ? "Even" : "Odd";
+  }
   render = () => (
     <h4 className="bg-primary text-white text-center p-2 m-1">
-      Component Element
+      Number of things: {this.isEven()}
     </h4>
   );
 }
