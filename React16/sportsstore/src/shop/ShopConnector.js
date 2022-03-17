e8bc48d41840a41433f2fdfbe80f884e97938282
ak <41680455+akudrin@users.noreply.github.com>
2022-03-17 19:25:02 +0000

    React16 - sportsstore Applying the New Components

--- React16/sportsstore/src/shop/ShopConnector.js
+++ React16/sportsstore/src/shop/ShopConnector.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
-import { loadData } from "../data/ActionCreators";
+import { loadData, placeOrder } from "../data/ActionCreators";
 import { DataTypes } from "../data/Types";
 import { Shop } from "./Shop";
 import {
@@ -12,6 +12,8 @@ import {
 } from "../data/CartActionCreators";
 import { CartDetails } from "./CartDetails";
 import { DataGetter } from "../data/DataGetter";
+import { Checkout } from "./Checkout";
+import { Thanks } from "./Thanks";
 const mapStateToProps = (dataStore) => ({
   ...dataStore,
 });
@@ -21,6 +23,7 @@ const mapDispatchToProps = {
   updateCartQuantity,
   removeFromCart,
   clearCart,
+  placeOrder,
 };
 // const filterProducts = (products = [], category) =>
 // (!category || category === "All")
@@ -53,6 +56,16 @@ export const ShopConnector = connect(
             render={(routeProps) => (
               <CartDetails {...this.props} {...routeProps} />
             )}
+          />
+          <Route
+            path="/shop/checkout"
+            render={(routeProps) => (
+              <Checkout {...this.props} {...routeProps} />
+            )}
+          />
+          <Route
+            path="/shop/thanks"
+            render={(routeProps) => <Thanks {...this.props} {...routeProps} />}
           />
           <Redirect to="/shop/products/all/1" />
         </Switch>
