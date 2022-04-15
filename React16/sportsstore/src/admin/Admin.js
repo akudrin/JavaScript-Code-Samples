32816630d754dba36cebe02dc12da91f0f07bde4
ak <41680455+akudrin@users.noreply.github.com>
2022-04-15 23:13:09 +0100

    React16 - Enabling Lazy Loading for Admin 2

--- React16/sportsstore/src/admin/Admin.js
+++ React16/sportsstore/src/admin/Admin.js
@@ -13,7 +13,7 @@ import { authWrapper } from "../auth/AuthWrapper";
 // const graphQlClient = new ApolloClient({
 // uri: GraphQlUrl
 // });
-export const Admin = authWrapper(
+export default authWrapper(
   class extends Component {
     constructor(props) {
       super(props);
