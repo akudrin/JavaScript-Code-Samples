eedc92619faea0336f723c44159a9c6a4aeca067
ak <41680455+akudrin@users.noreply.github.com>
2022-04-20 22:41:48 +0100

    React16 - Understanding Static Content

--- React16/projecttools/src/App.js
+++ React16/projecttools/src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
+import "./App.css";
+import reactLogo from "./logo.svg";
 let name = "Adam";
 const city = "London";
 export default class extends Component {
   message = () => `Hello ${name} from ${city}`;
   render = () => (
-    <h4 className="bg-primary text-white text-center p-3">{this.message()}</h4>
+    <div className="text-center">
+      <h4 className="bg-primary text-white text-center p-3">
+        {this.message()}
+      </h4>
+      <img src={reactLogo} alt="reactLogo" />
+    </div>
   );
 }
