ab039d8a0672ffa397a8ffde77259feb8904509c
ak <41680455+akudrin@users.noreply.github.com>
2022-04-25 20:50:53 +0100

    React16 - Rendering Other Components

--- React16/components/src/App.js
+++ React16/components/src/App.js
@@ -1,5 +1,14 @@
 import React from "react";
-export const App = () => (
-  <h1 className="bg-primary text-white text-center p-2">Hello Adam</h1>
-);
-export default App;
+import { Message } from "./Message";
+import { Summary } from "./Summary";
+export default function App() {
+  return (
+    <div>
+      <h1 className="bg-primary text-white text-center p-2">Hello Adam</h1>
+      <Message />
+      <Message />
+      <Message />
+      <Summary />
+    </div>
+  );
+}
