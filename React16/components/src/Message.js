b73ca6feb6f7bde330e76341b86cb0ab2aa8158c
ak <41680455+akudrin@users.noreply.github.com>
2022-04-26 19:12:13 +0100

    React16 - props intro

--- React16/components/src/Message.js
+++ React16/components/src/Message.js
@@ -1,6 +1,8 @@
 import React from "react";
-export function Message() {
+export function Message(props) {
   return (
-    <h4 className="bg-success text-white text-center p-2">This is a message</h4>
+    <h4 className="bg-success text-white text-center p-2">
+      {props.greeting}, {props.name}
+    </h4>
   );
 }
