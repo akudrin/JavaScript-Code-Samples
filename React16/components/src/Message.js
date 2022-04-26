7fd41e552f42b448e657a3c9144da846d2f7a9ce
ak <41680455+akudrin@users.noreply.github.com>
2022-04-26 19:17:45 +0100

    React16 - Selecting a Property Value

--- React16/components/src/Message.js
+++ React16/components/src/Message.js
@@ -1,16 +1,12 @@
 import React from "react";
 export function Message(props) {
-  if (props.name === "Bob") {
-    return (
-      <h4 className="bg-warning p-2">
-        {props.greeting}, {props.name}
-      </h4>
-    );
-  } else {
-    return (
-      <h4 className="bg-success text-white text-center p-2">
-        {props.greeting}, {props.name}
-      </h4>
-    );
-  }
+  let classes =
+    props.name === "Bob"
+      ? "bg-warning p-2"
+      : "bg-success text-white text-center p-2";
+  return (
+    <h4 className={classes}>
+      {props.greeting}, {props.name}
+    </h4>
+  );
 }
