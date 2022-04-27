531bde300887e61dfb29d108d7a55a6ec992c86e
ak <41680455+akudrin@users.noreply.github.com>
2022-04-27 22:41:16 +0100

    React16 - Using the Map Method to Process Array Objects 2

--- React16/components/src/Summary.js
+++ React16/components/src/Summary.js
@@ -1,13 +1,10 @@
 import React from "react";
-function createInnerElements(names) {
-  return names.map((name) => (
-    <div>{`${name} contains ${name.length} letters`}</div>
-  ));
-}
 export function Summary(props) {
   return (
     <h4 className="bg-info text-white text-center p-2">
-      {createInnerElements(props.names)}
+      {props.names.map((name) => (
+        <div>{`${name} contains ${name.length} letters`}</div>
+      ))}
     </h4>
   );
 }
