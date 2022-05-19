47b037198abc9ba9e38c80fdc6157ab782aea292
ak <41680455+akudrin@users.noreply.github.com>
2022-05-19 19:00:10 +0100

    React16 - Understanding the Target and Bubble Phases 2

--- React16/reactevents/src/App.js
+++ React16/reactevents/src/App.js
@@ -6,44 +6,32 @@ export default class App extends Component {
     this.state = {
       message: "Ready",
       counter: 0,
-      theme: "info",
+      theme: "secondary",
     };
   }
-
-  handleEvent = (event, newTheme) => {
-    event.persist();
-    this.setState(
-      {
-        counter: this.state.counter + 1,
-        theme: newTheme,
-      },
-      () => this.setState({ message: `${event.type}: ${this.state.counter}` })
-    );
-  };
-
-  toggleCheckBox = (event) => {
-    if (this.state.counter === 0) {
-      event.preventDefault();
-    }
-  };
-
   selectTheme = (newTheme) => {
     this.setState({
       theme: newTheme,
       message: `Theme: ${newTheme}`,
     });
   };
-
+  handleClick = (event) => {
+    console.log(
+      `App: Type: ${event.type} ` +
+        `Target: ${event.target.tagName} ` +
+        `CurrentTarget: ${event.currentTarget.tagName}`
+    );
+  };
   render() {
     return (
-      <div className="m-2">
+      <div className="m-2" onClick={this.handleClick}>
         <div
           className={`h4 bg-${this.state.theme}
-    text-white text-center p-2`}
+  text-white text-center p-2`}
         >
           {this.state.message}
         </div>
-        <div className="text-center">
+        <div className="text-center" onClick={this.handleClick}>
           <ThemeButton theme="primary" callback={this.selectTheme} />
           <ThemeButton theme="danger" callback={this.selectTheme} />
         </div>
