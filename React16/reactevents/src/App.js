2896c2d65f11072bcae5bf04afbe7a05025ca421
ak <41680455+akudrin@users.noreply.github.com>
2022-05-19 18:39:26 +0100

    React16 - Understanding the Target and Bubble Phases 1

--- React16/reactevents/src/App.js
+++ React16/reactevents/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { ThemeButton } from "./ThemeButton";
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -26,17 +27,16 @@ export default class App extends Component {
     }
   };
 
+  selectTheme = (newTheme) => {
+    this.setState({
+      theme: newTheme,
+      message: `Theme: ${newTheme}`,
+    });
+  };
+
   render() {
     return (
       <div className="m-2">
-        <div className="form-check">
-          <input
-            className="form-check-input"
-            type="checkbox"
-            onClick={this.toggleCheckBox}
-          />
-          <label>This is a checkbox</label>
-        </div>
         <div
           className={`h4 bg-${this.state.theme}
     text-white text-center p-2`}
@@ -44,18 +44,8 @@ export default class App extends Component {
           {this.state.message}
         </div>
         <div className="text-center">
-          <button
-            className="btn btn-primary"
-            onClick={(e) => this.handleEvent(e, "primary")}
-          >
-            Normal
-          </button>
-          <button
-            className="btn btn-danger m-1"
-            onClick={(e) => this.handleEvent(e, "danger")}
-          >
-            Danger
-          </button>
+          <ThemeButton theme="primary" callback={this.selectTheme} />
+          <ThemeButton theme="danger" callback={this.selectTheme} />
         </div>
       </div>
     );
---
+++ React16/reactevents/src/ThemeButton.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+export class ThemeButton extends Component {
+  handleClick = (event) => {
+    console.log(
+      `Will this event bubble: ${event.bubbles} ` +
+        `ThemeButton: Type: ${event.type} ` +
+        `Target: ${event.target.tagName} ` +
+        `CurrentTarget: ${event.currentTarget.tagName}`
+    );
+    this.props.callback(this.props.theme);
+  };
+  render() {
+    return (
+      <span className="m-1" onClick={this.handleClick}>
+        <button
+          className={`btn btn-${this.props.theme}`}
+          onClick={this.handleClick}
+        >
+          Select {this.props.theme} Theme
+        </button>
+      </span>
+    );
+  }
+}
