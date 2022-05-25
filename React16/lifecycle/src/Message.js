8f0af7f1078fc485466801ed45c1364db799c919
ak <41680455+akudrin@users.noreply.github.com>
2022-05-25 18:57:41 +0100

    React16 - Changing Content in the Message.js

--- React16/lifecycle/src/Message.js
+++ React16/lifecycle/src/Message.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import { ActionButton } from "./ActionButton";
+//document.getElementById("messageDiv").classList.add("bg-info")
 export class Message extends Component {
   render() {
     console.log(`Render Message Component `);
     return (
       <div>
         <ActionButton theme="primary" {...this.props} />
-        <div className="h5 text-center p-2">{this.props.message}</div>
+        <div id="messageDiv" className="h5 text-center p-2">
+          {this.props.message}
+        </div>
       </div>
     );
   }
