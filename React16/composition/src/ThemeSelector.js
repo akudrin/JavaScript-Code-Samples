d970f819ef1b94d2311d8ed226aba396c3c12e5a
ak <41680455+akudrin@users.noreply.github.com>
2022-06-04 16:28:17 +0100

    React16 - Using the Children Prop

---
+++ React16/composition/src/ThemeSelector.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+export class ThemeSelector extends Component {
+  render() {
+    return (
+      <div className="bg-dark p-2">
+        <div className="bg-info p-2">{this.props.children}</div>
+      </div>
+    );
+  }
+}
