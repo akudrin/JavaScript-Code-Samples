5e3e3e960224c4d19ad7701ae9425803d2a5a9d9
ak <41680455+akudrin@users.noreply.github.com>
2022-06-06 17:55:38 +0100

    React16 - Changing the order of child components in a container

--- React16/composition/src/ThemeSelector.js
+++ React16/composition/src/ThemeSelector.js
@@ -4,18 +4,28 @@ export class ThemeSelector extends Component {
     super(props);
     this.state = {
       theme: "primary",
+      reverseChildren: false,
     };
     this.themes = ["primary", "secondary", "success", "warning", "dark"];
   }
   setTheme = (event) => {
     this.setState({ theme: event.target.value });
   };
+  toggleReverse = () => {
+    this.setState({ reverseChildren: !this.state.reverseChildren });
+  };
   render() {
     let modChildren = React.Children.map(this.props.children, (c) =>
       React.cloneElement(c, { theme: this.state.theme })
     );
+    if (this.state.reverseChildren) {
+      modChildren.reverse();
+    }
     return (
       <div className="bg-dark p-2">
+        <button className="btn btn-primary" onClick={this.toggleReverse}>
+          Reverse
+        </button>
         <div className="form-group text-left">
           <label className="text-white">Theme:</label>
           <select
