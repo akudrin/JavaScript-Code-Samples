9d287089a6ab665552bab8d313ed2324881653b3
ak <41680455+akudrin@users.noreply.github.com>
2022-06-09 16:22:41 +0100

    React16 - Creating Stateful Higher-Order Components

--- React16/composition/src/App.js
+++ React16/composition/src/App.js
@@ -1,48 +1,33 @@
 import React, { Component } from "react";
 import { GeneralList } from "./GeneralList";
 import { SortedList } from "./SortedList";
-import { ProFeature } from "./ProFeature";
-const ProList = ProFeature(SortedList);
+//import { ProFeature } from "./ProFeature";
+import { ProController } from "./ProController";
+const ProList = ProController(SortedList);
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       names: ["Zoe", "Bob", "Alice", "Dora", "Joe"],
       cities: ["London", "New York", "Paris", "Milan", "Boston"],
-      proMode: false,
+      //proMode: false
     };
   }
-  toggleProMode = () => {
-    this.setState({ proMode: !this.state.proMode });
-  };
   render() {
     return (
       <div className="container-fluid">
-        <div className="row">
-          <div className="col-12 text-center p-2">
-            <div className="form-check">
-              <input
-                type="checkbox"
-                className="form-check-input"
-                value={this.state.proMode}
-                onChange={this.toggleProMode}
-              />
-              <label className="form-check-label">Pro Mode</label>
-            </div>
-          </div>
-        </div>
         <div className="row">
           <div className="col-3">
             <GeneralList list={this.state.names} theme="primary" />
           </div>
           <div className="col-3">
-            <ProList list={this.state.names} pro={this.state.proMode} />
+            <ProList list={this.state.names} />
           </div>
           <div className="col-3">
             <GeneralList list={this.state.cities} theme="secondary" />
           </div>
           <div className="col-3">
-            <ProList list={this.state.cities} pro={this.state.proMode} />
+            <ProList list={this.state.cities} />
           </div>
         </div>
       </div>
