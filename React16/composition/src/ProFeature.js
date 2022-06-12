2a94cb7cd18ea417bc90ca590ff39b41611ba950
ak <41680455+akudrin@users.noreply.github.com>
2022-06-12 18:03:27 +0100

    React16 - Using a Render Prop with an Argument

--- React16/composition/src/ProFeature.js
+++ React16/composition/src/ProFeature.js
@@ -1,7 +1,7 @@
 import React from "react";
 export function ProFeature(props) {
   if (props.pro) {
-    return props.render();
+    return props.render("Pro Feature");
   } else {
     return (
       <h5 className="bg-warning text-white text-center">
