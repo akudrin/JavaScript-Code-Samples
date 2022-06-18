dbd23a1d7b8393ed7dd79a3eea5354a186a020dd
ak <41680455+akudrin@users.noreply.github.com>
2022-06-18 13:28:41 +0100

    React16 - Changing Context Data Values in a Consumer 1

--- React16/composition/src/App.js
+++ React16/composition/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { GeneralList } from "./GeneralList";
 import { SortedList } from "./SortedList";
+import { ProModeContext } from "./ProModeContext";
+import { ProModeToggle } from "./ProModeToggle";
 //import { ProFeature } from "./ProFeature";
 // import { ProController } from "./ProController";
 // import { LogToConsole } from "./LogToConsole";
@@ -11,36 +13,36 @@ export default class App extends Component {
     this.state = {
       names: ["Zoe", "Bob", "Alice", "Dora", "Joe"],
       cities: ["London", "New York", "Paris", "Milan", "Boston"],
-      proMode: false,
+      //proMode: false,
+      proContextData: {
+        proMode: false,
+        toggleProMode: this.toggleProMode,
+      },
     };
   }
   toggleProMode = () => {
-    this.setState({ proMode: !this.state.proMode });
+    this.setState(
+      (state) => (state.proContextData.proMode = !state.proContextData.proMode)
+    );
   };
   render() {
     return (
       <div className="container-fluid">
-        <div className="row">
-          <div className="col-12 text-center p-2">
-            <div className="form-check">
-              <input
-                type="checkbox"
-                className="form-check-input"
-                value={this.state.proMode}
-                onChange={this.toggleProMode}
-              />
-              <label className="form-check-label">Pro Mode</label>
+        <ProModeContext.Provider value={this.state.proContextData}>
+          <div className="row">
+            <div className="col-12 text-center p-2">
+              <ProModeToggle label="Pro Mode" />
             </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-6">
-            <GeneralList list={this.state.names} theme="primary" />
+          <div className="row">
+            <div className="col-6">
+              <GeneralList list={this.state.names} theme="primary" />
+            </div>
+            <div className="col-6">
+              <SortedList list={this.state.names} />
+            </div>
           </div>
-          <div className="col-6">
-            <SortedList proMode={this.state.proMode} list={this.state.names} />
-          </div>
-        </div>
+        </ProModeContext.Provider>
       </div>
     );
   }
