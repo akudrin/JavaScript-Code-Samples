8b7a9f4085860503b98dea5a4ab5ea38a681fbd3
ak <41680455+akudrin@users.noreply.github.com>
2022-06-29 13:18:46 +0100

    React16 - Using Text Areas

--- React16/forms/src/Editor.js
+++ React16/forms/src/Editor.js
@@ -3,35 +3,13 @@ export class Editor extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "",
-      flavor: "Vanilla",
-      toppings: ["Strawberries"],
+      name: "Bob",
+      order: "",
     };
-    this.flavors = [
-      "Chocolate",
-      "Double Chocolate",
-      "Triple Chocolate",
-      "Vanilla",
-    ];
-    this.toppings = ["Sprinkles", "Fudge Sauce", "Strawberries", "Maple Syrup"];
   }
   updateFormValue = (event) => {
     this.setState({ [event.target.name]: event.target.value }, () =>
       this.props.submit(this.state)
-    );
-  };
-  updateFormValueCheck = (event) => {
-    event.persist();
-    this.setState(
-      (state) => {
-        if (event.target.checked) {
-          state.toppings.push(event.target.name);
-        } else {
-          let index = state.toppings.indexOf(event.target.name);
-          state.toppings.splice(index, 1);
-        }
-      },
-      () => this.props.submit(this.state)
     );
   };
   render() {
@@ -47,20 +25,13 @@ export class Editor extends Component {
           />
         </div>
         <div className="form-group">
-          <label>Ice Cream Toppings</label>
-          {this.toppings.map((top) => (
-            <div className="form-check" key={top}>
-              <input
-                className="form-check-input"
-                type="checkbox"
-                name={top}
-                value={this.state[top]}
-                checked={this.state.toppings.indexOf(top) > -1}
-                onChange={this.updateFormValueCheck}
-              />
-              <label className="form-check-label">{top}</label>
-            </div>
-          ))}
+          <label>Order</label>
+          <textarea
+            className="form-control"
+            name="order"
+            value={this.state.order}
+            onChange={this.updateFormValue}
+          />
         </div>
       </div>
     );
