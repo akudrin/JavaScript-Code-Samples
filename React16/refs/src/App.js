fc5cfc0c3c040c3dc40b4928dfd33e66c2730406
ak <41680455+akudrin@users.noreply.github.com>
2022-07-12 18:24:33 +0100

    React16 - Accessing a Child Component’s Content

--- React16/refs/src/App.js
+++ React16/refs/src/App.js
@@ -1,29 +1,21 @@
 import React, { Component } from "react";
-import { Editor } from "./Editor";
-import { ProductTable } from "./ProductTable";
+import { FormField } from "./FormField";
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      products: [],
-    };
+    this.fieldRef = React.createRef();
   }
-  addProduct = (product) => {
-    if (this.state.products.indexOf(product.name) === -1) {
-      this.setState({ products: [...this.state.products, product] });
-    }
+  handleClick = () => {
+    this.fieldRef.current.focus();
   };
   render() {
     return (
-      <div>
-        <Editor callback={this.addProduct} />
-        <h6 className="bg-secondary text-white m-2 p-2">Products</h6>
-        <div className="m-2">
-          {this.state.products.length === 0 ? (
-            <div className="text-center">No Products</div>
-          ) : (
-            <ProductTable products={this.state.products} />
-          )}
+      <div className="m-2">
+        <FormField label="Name" fieldRef={this.fieldRef} />
+        <div className="text-center m-2">
+          <button className="btn btn-primary" onClick={this.handleClick}>
+            Focus
+          </button>
         </div>
       </div>
     );
