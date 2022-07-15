812d902515c0e2282940b81acdf1bead5917799a
ak <41680455+akudrin@users.noreply.github.com>
2022-07-15 14:13:39 +0100

    React16 - Using Portals

--- React16/forms/src/App.js
+++ React16/forms/src/App.js
@@ -1,26 +1,32 @@
 import React, { Component } from "react";
-import { Editor } from "./Editor";
-import { Display } from "./Display";
+import { ForwardFormField } from "./FormField";
+import { PortalWrapper } from "./PortalWrapper";
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      formData: {},
-    };
+    this.fieldRef = React.createRef();
+    this.portalFieldRef = React.createRef();
   }
-  submitData = (newData) => {
-    this.setState({ formData: newData });
+  focusLocal = () => {
+    this.fieldRef.current.focus();
+  };
+  focusPortal = () => {
+    this.portalFieldRef.current.focus();
   };
   render() {
     return (
-      <div className="container-fluid">
-        <div className="row p-2">
-          <div className="col-6">
-            <Editor submit={this.submitData} />
-          </div>
-          <div className="col-6">
-            <Display data={this.state.formData} />
-          </div>
+      <div>
+        <PortalWrapper>
+          <ForwardFormField label="Name" ref={this.portalFieldRef} />
+        </PortalWrapper>
+        <ForwardFormField label="Name" ref={this.fieldRef} />
+        <div className="text-center m-2">
+          <button className="btn btn-primary m-1" onClick={this.focusLocal}>
+            Focus Local
+          </button>
+          <button className="btn btn-primary m-1" onClick={this.focusPortal}>
+            Focus Portal
+          </button>
         </div>
       </div>
     );
